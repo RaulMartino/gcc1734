506c06450fe54b941f50741b8d81e2a5eb47ba1e
RaulMartino <83245964+RaulMartino@users.noreply.github.com>
2024-09-02 20:58:41 -0300

    Update cliffwalking_feature_extractor

--- src/rl/cliffwalking_feature_extractor.py
+++ src/rl/cliffwalking_feature_extractor.py
@@ -16,52 +16,53 @@ class CliffWalkingFeatureExtractor(FeatureExtractor):
     }
 
     def __init__(self, env):
-        """
+        '''
         Initializes the CliffWalkingFeatureExtractor object.
-        Adds feature extraction methods to the features_list attribute.
-        """
+        It adds feature extraction methods to the features_list attribute.
+        '''
         self.env = env
         self.features_list = []
-        self.features_list.append(self.f0)  # Bias term
-        self.features_list.append(self.f1)  # Penalidade por proximidade ao penhasco
-        self.features_list.append(self.f2)  # Distância até a meta
+        self.features_list.append(self.f0)
+        self.features_list.append(self.f1)
+        self.features_list.append(self.f2)
+        self.features_list.append(self.f3)
 
     def get_num_features(self):
-        """
+        '''
         Returns the number of features extracted by the feature extractor.
-        """
+        '''
         return len(self.features_list) + self.get_num_actions()
 
     def get_num_actions(self):
-        """
+        '''
         Returns the number of actions available in the environment.
-        """
+        '''
         return len(self.get_actions())
 
     def get_action_one_hot_encoded(self, action):
-        """
+        '''
         Returns the one-hot encoded representation of an action.
-        """
+        '''
         return self.__actions_one_hot_encoding[action]
 
     def is_terminal_state(self, state):
-        """
-        Checks if the state is terminal.
-        The terminal state is when the agent reaches the goal at [3, 11].
-        """
-        return state == 47
+        '''
+        Checks if the given state is a terminal state.
+        In CliffWalking, the terminal state is the goal position (3, 11).
+        '''
+        return state == (3, 11)
 
     def get_actions(self):
-        """
+        '''
         Returns a list of available actions in the environment.
-        """
+        '''
         return [Actions.UP, Actions.RIGHT, Actions.DOWN, Actions.LEFT]
     
     def get_features(self, state, action):
-        """
+        '''
         Takes a state and an action as input and returns the feature vector for that state-action pair.
         It calls the feature extraction methods and constructs the feature vector.
-        """
+        '''
         feature_vector = np.zeros(len(self.features_list))
         for index, feature in enumerate(self.features_list):
             feature_vector[index] = feature(state, action)
@@ -72,19 +73,39 @@ class CliffWalkingFeatureExtractor(FeatureExtractor):
         return feature_vector
 
     def f0(self, state, action):
-        """
+        '''
         This is just the bias term.
-        """
+        '''
         return 1.0
 
     def f1(self, state, action):
-        cliff_positions = [37, 38, 39, 40, 41, 42, 43, 44, 45, 46]  # Penhasco na linha 3, colunas 1 a 10
-        penalty = 1.0 if state in cliff_positions else 0.0
-        return -penalty  # Penalidade para estados no penhasco
+        '''
+        Distance to goal feature.
+        '''
+        goal = (3, 11)
+        position = (state // 12, state % 12)
+        distance = self.__manhattanDistance(position, goal)
+        return 1.0 / (distance + 1)  # Adding 1 to avoid division by zero
 
     def f2(self, state, action):
-        goal_position = (3, 11)
-        agent_position = (state // 12, state % 12)
-        distance_to_goal = np.linalg.norm(np.array(goal_position) - np.array(agent_position))
-        max_distance = np.linalg.norm(np.array([0, 0]) - np.array(goal_position))
-        return 1.0 - (distance_to_goal / max_distance)
+        '''
+        Proximity to cliff feature.
+        '''
+        position = (state // 12, state % 12)
+        if position[0] == 3 and 1 <= position[1] <= 10:
+            return 1.0
+        return 0.0
+
+    def f3(self, state, action):
+        '''
+        Progress along x-axis feature.
+        '''
+        position = (state // 12, state % 12)
+        return position[1] / 11.0  # Normalize by grid width
+
+    @staticmethod
+    def __manhattanDistance(xy1, xy2):
+        '''
+        Computes the Manhattan distance between two points.
+        '''
+        return abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
